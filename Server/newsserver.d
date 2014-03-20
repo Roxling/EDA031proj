3b5c6cd15bf49b4d8be68e3dcda2cfc74c5f707d
Valdemar Roxling <dat11vro@val-5.student.lth.se>
2014-03-20 17:06:21 +0100

    u fail..

--- Server/newsserver.d
+++
@@ -1,4 +0,0 @@
-newsserver.o newsserver.d : newsserver.cc server.h ../connection.h \
- ../connectionclosedexception.h ../protocol.h Commands/CommandFactory.h \
- Commands/Command.h Commands/../../protocol.h DB/Database.h DB/Article.h \
- DB/NewsGroup.h
