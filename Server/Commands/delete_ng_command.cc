9811fd9bb33783b549281fb2c98db51c822d3408
Valdemar Roxling <dat11vro@hacke-6.student.lth.se>
2014-03-26 12:58:37 +0100

    delete ng

--- Server/Commands/delete_ng_command.cc
+++ Server/Commands/delete_ng_command.cc
@@ -2,30 +2,23 @@
 #include "Command.h"
 
 delete_ng_command::delete_ng_command(shared_ptr<Database>& db2, shared_ptr<Connection>& c) : Command(db2,c){
-	try{
-		if(conn->read() != protocol.PAR_NUM)protocolBroken();
-		int n  = conn->read();
-		ngID = to_string(n);
-		if(conn->read() != protocol.COM_END)protocolBroken();
-	}catch(...){
-		std::cout << "Caught a villanous exception in delete_NG!" << std::endl;
+	if(conn->read() == protocol.PAR_NUM){
+		ngID = to_string(readNumber(*conn));
+	}else{
+		protocolBroken();
+	}
+	if(conn->read() != protocol.COM_END){
 		protocolBroken();
 	}
 }
 
 void delete_ng_command::exec(){
-	try{
-	   db->removeNewsGroup(ngID);
-	}catch(...){
-	   protocolBroken();
-
-	   conn->write(protocol.ANS_DELETE_NG);
-	   conn->write(protocol.ANS_NAK);
-	   conn->write(protocol.ERR_NG_DOES_NOT_EXIST);
-	   conn->write(protocol.ANS_END);
-	}
-	//deal with errors
 	conn->write(protocol.ANS_DELETE_NG);
-	conn->write(protocol.ANS_ACK);
+	if(db->removeNewsGroup(ngID)){
+		conn->write(protocol.ANS_ACK);
+	}else{
+		conn->write(protocol.ANS_NAK);
+		conn->write(protocol.ERR_NG_DOES_NOT_EXIST);
+	}
 	conn->write(protocol.ANS_END);
 }
